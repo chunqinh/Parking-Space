3f11d7529b68d4dae1327f6d360a872469acb576
swisstackle <alainsch@buffalo.edu>
2021-09-28 18:48:47 -0400

    Issue #21, Blobs added

--- frontend/src/components/pages/sign-in.jsx
+++ frontend/src/components/pages/sign-in.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import blob from '../icons/login-blob.svg'
+import { ReactComponent as blob2 } from "../icons/login-blob.svg";
+
 import LoginForm from "../form/login";
 
 function Login(){
@@ -13,8 +15,21 @@ function Login(){
                     <div className="single-height-blob">
                         <div style={{width:'400px', textAlign:'right'}}>
                             <h1 className="super-heading">TROUBLE FINDING A SPOT?</h1>
+
                         </div>
+
+
                     </div>
+                    <div className="absolute-blob">
+                        <img src={blob}/>
+                    </div>
+                    <div className="absolute-blob2">
+                        <img src={blob}/>
+                    </div>
+                    <div className="absolute-blob3">
+                        <img src={blob}/>
+                    </div>
+
                 </div>
             </div>
         </div>
