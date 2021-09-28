1179fad2f1748716fa2ab60ee804fb46c945e6c8
swisstackle <alainsch@buffalo.edu>
2021-09-28 18:43:18 -0400

    Issue #21: Blobs added on login page. Showpassword feature coming next

--- frontend/src/components/form/login.jsx
+++ frontend/src/components/form/login.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from 'react';
+// import IconButton from "@material-ui/core/IconButton";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import Visibility from "@material-ui/icons/Visibility";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+// import VisibilityOff from "@material-ui/icons/VisibilityOff";
+// import Input from "@material-ui/core/Input";
 
+let showPassword = false;
+function handleShowPassword(){
+    showPassword = !showPassword;
+}
 function LoginForm(){
+
+
     return(
         <form className="login-form">
             <div>
@@ -9,7 +21,7 @@ function LoginForm(){
             </div>
             <div>
                 <h6 className="labels">PASSWORD:</h6>
-                <input type={"password"}/>
+                <input type={showPassword ? "text":"password"}/>
             </div>
             <div style={{display:'flex',flexDirection:'column', marginTop:'24px', alignItems:'center'}}>
                 <button type={"submit"} className="login-button"> LOGIN </button>
