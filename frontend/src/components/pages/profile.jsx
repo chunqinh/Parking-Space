0e6682ba9cdf4161cb368b53b62b07c2fd0cbc7f
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-10-30 19:44:53 -0400

    Issue #41 - Edit Schedule static feature completed

--- frontend/src/components/pages/profile.jsx
+++ frontend/src/components/pages/profile.jsx
@@ -63,6 +63,7 @@ export default function UserProfile() {
                 <TabPanel value={value} index={2}>
                     <ProfilePasswordChange/>
                 </TabPanel>
+
             </div>
 
         </div>
