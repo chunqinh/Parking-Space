cc459147faccdc182c0bad8c47440cc6e5f92aa1
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-11-08 23:58:56 -0500

    Issue #56 Proxy removed

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware(["/api", undefined, "/otherApi"], { target: "https://parkapp-space-442-backend.herokuapp.com/" })
-    );
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+//
+// module.exports = function(app) {
+//     app.use(
+//         createProxyMiddleware(["/node", undefined, "/node"], { target: "https://parkapp-space-442-backend.herokuapp.com/" })
+//     );
+// };
