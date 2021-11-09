f2fc4801fddbd4c0105812f71ba710a89db8dd02
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-11-09 01:42:01 -0500

    Issue #56 Delete proxy.js

--- frontend/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-//
-// module.exports = function(app) {
-//     app.use(
-//         createProxyMiddleware(["/node", undefined, "/node"], { target: "https://parkapp-space-442-backend.herokuapp.com/" })
-//     );
-// };
