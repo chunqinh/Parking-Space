d28f76f6cb4c455e9b156f10bd889a3b8077455b
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-11-09 03:25:05 -0500

    Issue #56 server merged and connected

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const db = require('./database');
 const cors = require('cors')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
