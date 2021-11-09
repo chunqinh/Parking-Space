545d077b48e5d980165336dc174ed130c70a460f
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-11-09 14:46:53 -0500

    Issue #56 Edited dockerfile to run a script

---
+++ backend/myscript.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+npm start
+npm frontend
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8081;
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname,'\.env')});
 const db = require('./database');
 const cors = require('cors')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -19,6 +18,30 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 //     })
 // );
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8081', 'https://parking-space-442.herokuapp.com']
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+// Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+}
 
 app.get('/', function (req,res){
     res.send("Hello World");
