1a61f55a19c107abcf94703309ba0033a9ee1c10
swisstackle <alainsch@buffalo.edu>
2021-11-10 16:19:59 -0500

    #55 Testing added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import index from './components/pages/home.jsx';
+import index1 from './components/pages/home.jsx';
 import dashboard from './components/pages/dashboard.jsx';
 
 it('Home Page test', () => {
-    const tree = renderer.create(<index />).toJSON();
+    const tree = renderer.create(<index1 />).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
