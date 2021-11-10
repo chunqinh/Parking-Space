8b39baf44757a8ab52307a15ef74535706227753
Prasoon Naithani <prasoonnaithani@gmail.com>
2021-11-10 16:02:36 -0500

    Issue #1 #13 #2 Faculty filter added

--- frontend/src/components/google-map-api/map.jsx
+++ frontend/src/components/google-map-api/map.jsx
@@ -7,25 +7,29 @@ const containerStyle = {
 };
 
 const student_parking_lots = {
-    "Alumni A Lot" : [43.000716, -78.780223, true,"https://goo.gl/maps/z4SbM4aJdfuB5HwV6"],
-    "Alumni B Lot" : [43.001901, -78.77871, true,"https://goo.gl/maps/s33e9WZaAFmrB4Rz7"],
-    "Alumni C Lot" : [43.001909, -78.779976, true,"https://goo.gl/maps/Camn2CzybZpLsUcU9"],
-    "Arena Lot" : [43.000857, -78.779418, true,"https://goo.gl/maps/5pTg8WhP4RhjSy8X7"],
-    "Baird A Lot" : [42.998558, -78.784504, true,"https://goo.gl/maps/45MivZVFVq22L2Fh9"],
-    "Bookstore Lot": [43.002333,-78.78534, false,"https://goo.gl/maps/87c2krGjP7XR92qQ7"],
-    "Cooke A Lot" : [42.999437, -78.793216 ,true,"https://goo.gl/maps/Pk3XbMH2YNsZXkLj9"],
-    "Cooke B Lot" : [42.998715, -78.793237, true,"https://goo.gl/maps/dyMAes4BksPJ8FdZ7"],
-    "Crofts Lot" : [42.994807, -78.797078, true,"https://goo.gl/maps/GKfubFAw3Gjcy8pi7"],
-    "Fronczak Lot" : [43.002425, -78.791424, true,"https://goo.gl/maps/VchkxMUY14nd4Nwq8"],
-    "Hochstetter B Lot" : [42.99859, -78.790212, false,"https://goo.gl/maps/bUJnnMTF1eFhpxRc7"],
-    "Jacobs B Lot": [42.998401,-78.7871, true,"https://goo.gl/maps/qGXTVax2x9d6YNFj9"],
-    "Jacobs C Lot": [42.998574,-78.786113, true,"https://goo.gl/maps/Ha3VeTLKDvSWJCTB9"],
-    "Jarvis A Lot" : [43.003721, -78.788517, false,"https://goo.gl/maps/DJrwMmxD3FNmkjr96"],
-    "Jarvis B Lot" : [43.003972, -78.786929, true,"https://goo.gl/maps/pcVVLd6PnjKf1hZa9"],
-    "Ketter Lot" :  [43.002466, -78.788838, true,"https://goo.gl/maps/6h7i4edeea1Jd2BKA"],
-    "Park Hall Lot" : [42.999657, -78.788688, true,"https://goo.gl/maps/5ja1ArexJ4VQQ3UX7"],
-    "Slee A Lot" : [42.99848, -78.783517, true,"https://goo.gl/maps/3eavzUUU53MVKbmt9"],
-    "Slee B Lot" : [42.999374, -78.783474, true,"https://goo.gl/maps/qDQPpq8UavPQi75q6"]
+    "Alumni A Lot" : [43.000716, -78.780223, true,"https://goo.gl/maps/z4SbM4aJdfuB5HwV6", false],
+    "Alumni B Lot" : [43.001901, -78.77871, true,"https://goo.gl/maps/s33e9WZaAFmrB4Rz7",false],
+    "Alumni C Lot" : [43.001909, -78.779976, true,"https://goo.gl/maps/Camn2CzybZpLsUcU9",false],
+    "Arena Lot" : [43.000857, -78.779418, true,"https://goo.gl/maps/5pTg8WhP4RhjSy8X7",false],
+    "Baird A Lot" : [42.998558, -78.784504, true,"https://goo.gl/maps/45MivZVFVq22L2Fh9",false],
+    "Bookstore Lot": [43.002333,-78.78534, false,"https://goo.gl/maps/87c2krGjP7XR92qQ7",false],
+    "Cooke A Lot" : [42.999437, -78.793216 ,true,"https://goo.gl/maps/Pk3XbMH2YNsZXkLj9",false],
+    "Cooke B Lot" : [42.998715, -78.793237, true,"https://goo.gl/maps/dyMAes4BksPJ8FdZ7",false],
+    "Crofts Lot" : [42.994807, -78.797078, true,"https://goo.gl/maps/GKfubFAw3Gjcy8pi7",false],
+    "Furnas Lot" : [43.00245, -78.786328, true,"https://goo.gl/maps/sTBnFFY6f53cRRdH6", true],
+    "Fronczak Lot" : [43.002425, -78.791424, true,"https://goo.gl/maps/VchkxMUY14nd4Nwq8",false],
+    "Hochstetter A Lot" : [42.998809, -78.791585, true,"https://goo.gl/maps/9V5vYTvcDN2eT8Xx9", true],
+    "Hochstetter B Lot" : [42.99859, -78.790212, false,"https://goo.gl/maps/bUJnnMTF1eFhpxRc7",false],
+    "Governors A Lot" :[43.00234,-78.790877,true,"https://goo.gl/maps/28n38jT1GiTqfQqF9", true],
+    "Jacobs B Lot": [42.998401,-78.7871, true,"https://goo.gl/maps/qGXTVax2x9d6YNFj9",false],
+    "Jacobs A Lot": [42.998511,-78.788302, true,"https://goo.gl/maps/xgxEpQ4t3bUt9z6x6",true],
+    "Jacobs C Lot": [42.998574,-78.786113, true,"https://goo.gl/maps/Ha3VeTLKDvSWJCTB9",false],
+    "Jarvis A Lot" : [43.003721, -78.788517, false,"https://goo.gl/maps/DJrwMmxD3FNmkjr96",false],
+    "Jarvis B Lot" : [43.003972, -78.786929, true,"https://goo.gl/maps/pcVVLd6PnjKf1hZa9",false],
+    "Ketter Lot" :  [43.002466, -78.788838, true,"https://goo.gl/maps/6h7i4edeea1Jd2BKA",false],
+    "Park Hall Lot" : [42.999657, -78.788688, true,"https://goo.gl/maps/5ja1ArexJ4VQQ3UX7",false],
+    "Slee A Lot" : [42.99848, -78.783517, true,"https://goo.gl/maps/3eavzUUU53MVKbmt9",false],
+    "Slee B Lot" : [42.999374, -78.783474, true,"https://goo.gl/maps/qDQPpq8UavPQi75q6",false]
 }
 
 
@@ -41,7 +45,7 @@ const customStyle = [
     }
 ]
 
-function GoogleMaps({availables,frees,paids,nearbyFree,nearbyPaid}:{availables:boolean,frees:boolean,paids:boolean, nearbyFree:string,nearbyPaid:string}){
+function GoogleMaps({availables,frees,paids,nearbyFree,nearbyPaid,nearbyFaculty,faculty}:{availables:boolean,frees:boolean,paids:boolean, faculty:boolean,nearbyFaculty:string, nearbyFree:string,nearbyPaid:string}){
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API
@@ -84,33 +88,61 @@ function GoogleMaps({availables,frees,paids,nearbyFree,nearbyPaid}:{availables:b
             onLoad={onLoad}
             onUnmount={onUnmount}
         >
-            { /* Child components, such as markers, info windows, etc. */ }
-            {/*<Marker position={{lat: 43.0008, lng: -78.7891}}/>*/}
             {
                 currentPosition.lat &&
                 (
-                    <Marker position={currentPosition}/>
+                    <Marker position={currentPosition} onClick={() => {
+                        setSelectedCenter("Current Location");}}/>
 
                 )
             }
-            {Object.entries(student_parking_lots).map((key,value)=>{
+            {
+                currentPosition.lat && selectedCenter && (
+                    <InfoWindow
+                        onCloseClick={() => {
+                            setSelectedCenter(false);
+                        }}
+                        position={{
+                            lat: currentPosition.lat,
+                            lng: currentPosition.lng
+                        }}
+                    >
+                        <div>
+                            <h3>Current Location</h3>
+                            {/*<a href= {student_parking_lots[nearbyPaid][3]} target={"_blank"}> Navigate Here</a>*/}
+                        </div>
+
+                    </InfoWindow>
+                )
+            }
+            }
+            {
+
+                Object.entries(student_parking_lots).map((key,value)=>{
                 const positionMarker = {
                     lat : key[1][0],
                     lng : key[1][1]
                 }
 
                 if(paids && !frees){
-                    if(!key[1][2]){
+                    if(!key[1][2] && !key[1][4]){
                         return (<Marker position={positionMarker} icon={iconBase+'parking_lot_maps.png'}/>)
                     }
                 }
                 else if(frees && !paids){
-                    if(key[1][2]){
+                    if(key[1][2] && !key[1][4]){
+                        return (<Marker position={positionMarker} icon={iconBase+'parking_lot_maps.png'}/>)
+                    }
+                }
+                else if(faculty){
+                    if(key[1][4]){
                         return (<Marker position={positionMarker} icon={iconBase+'parking_lot_maps.png'}/>)
                     }
                 }
                 else{
-                    return (<Marker position={positionMarker} icon={iconBase+'parking_lot_maps.png'}/>)
+                    if(!key[1][4]){
+                        return (<Marker position={positionMarker} icon={iconBase+'parking_lot_maps.png'}/>)
+                    }
                 }
 
             })}
@@ -125,6 +157,11 @@ function GoogleMaps({availables,frees,paids,nearbyFree,nearbyPaid}:{availables:b
                 lng: student_parking_lots[nearbyPaid][1]}} onClick={() => {
                         setSelectedCenter(nearbyPaid);
                     }}/>
+                :
+                nearbyFaculty !== "" ? <Marker position={{lat: student_parking_lots[nearbyFaculty][0],
+                        lng: student_parking_lots[nearbyFaculty][1]}} onClick={() => {
+                        setSelectedCenter(nearbyFaculty);
+                        }}/>
                 :
                     <></>
             }
@@ -169,6 +206,26 @@ function GoogleMaps({availables,frees,paids,nearbyFree,nearbyPaid}:{availables:b
                 ) : <></>
 
             }
+            {
+                nearbyFaculty !== "" ? selectedCenter && (
+                    <InfoWindow
+                        onCloseClick={() => {
+                            setSelectedCenter(false);
+                        }}
+                        position={{
+                            lat: student_parking_lots[nearbyFaculty][0],
+                            lng: student_parking_lots[nearbyFaculty][1]
+                        }}
+                    >
+                        <div>
+                            <h3>{nearbyFaculty}</h3>
+                            <a href= {student_parking_lots[nearbyFaculty][3]} target={"_blank"}> Navigate Here</a>
+                        </div>
+
+                    </InfoWindow>
+                ) : <></>
+            }
+
         </GoogleMap>
     ) : <></>
 }
--- frontend/src/components/pages/dashboard.jsx
+++ frontend/src/components/pages/dashboard.jsx
@@ -3,34 +3,40 @@ import GoogleMaps from "../google-map-api/map";
 
 
 const parking = {
-    "Alumni A Lot" : [43.000716, -78.780223, true,"https://goo.gl/maps/z4SbM4aJdfuB5HwV6"],
-    "Alumni B Lot" : [43.001901, -78.77871, true,"https://goo.gl/maps/s33e9WZaAFmrB4Rz7"],
-    "Alumni C Lot" : [43.001909, -78.779976, true,"https://goo.gl/maps/Camn2CzybZpLsUcU9"],
-    "Arena Lot" : [43.000857, -78.779418, true,"https://goo.gl/maps/5pTg8WhP4RhjSy8X7"],
-    "Baird A Lot" : [42.998558, -78.784504, true,"https://goo.gl/maps/45MivZVFVq22L2Fh9"],
-    "Bookstore Lot": [43.002333,-78.78534, false,"https://goo.gl/maps/87c2krGjP7XR92qQ7"],
-    "Cooke A Lot" : [42.999437, -78.793216 ,true,"https://goo.gl/maps/Pk3XbMH2YNsZXkLj9"],
-    "Cooke B Lot" : [42.998715, -78.793237, true,"https://goo.gl/maps/dyMAes4BksPJ8FdZ7"],
-    "Crofts Lot" : [42.994807, -78.797078, true,"https://goo.gl/maps/GKfubFAw3Gjcy8pi7"],
-    "Fronczak Lot" : [43.002425, -78.791424, true,"https://goo.gl/maps/VchkxMUY14nd4Nwq8"],
-    "Hochstetter B Lot" : [42.99859, -78.790212, false,"https://goo.gl/maps/bUJnnMTF1eFhpxRc7"],
-    "Jacobs B Lot": [42.998401,-78.7871, true,"https://goo.gl/maps/qGXTVax2x9d6YNFj9"],
-    "Jacobs C Lot": [42.998574,-78.786113, true,"https://goo.gl/maps/Ha3VeTLKDvSWJCTB9"],
-    "Jarvis A Lot" : [43.003721, -78.788517, false,"https://goo.gl/maps/DJrwMmxD3FNmkjr96"],
-    "Jarvis B Lot" : [43.003972, -78.786929, true,"https://goo.gl/maps/pcVVLd6PnjKf1hZa9"],
-    "Ketter Lot" :  [43.002466, -78.788838, true,"https://goo.gl/maps/6h7i4edeea1Jd2BKA"],
-    "Park Hall Lot" : [42.999657, -78.788688, true,"https://goo.gl/maps/5ja1ArexJ4VQQ3UX7"],
-    "Slee A Lot" : [42.99848, -78.783517, true,"https://goo.gl/maps/3eavzUUU53MVKbmt9"],
-    "Slee B Lot" : [42.999374, -78.783474, true,"https://goo.gl/maps/qDQPpq8UavPQi75q6"]
+    "Alumni A Lot" : [43.000716, -78.780223, true,"https://goo.gl/maps/z4SbM4aJdfuB5HwV6", false],
+    "Alumni B Lot" : [43.001901, -78.77871, true,"https://goo.gl/maps/s33e9WZaAFmrB4Rz7",false],
+    "Alumni C Lot" : [43.001909, -78.779976, true,"https://goo.gl/maps/Camn2CzybZpLsUcU9",false],
+    "Arena Lot" : [43.000857, -78.779418, true,"https://goo.gl/maps/5pTg8WhP4RhjSy8X7",false],
+    "Baird A Lot" : [42.998558, -78.784504, true,"https://goo.gl/maps/45MivZVFVq22L2Fh9",false],
+    "Bookstore Lot": [43.002333,-78.78534, false,"https://goo.gl/maps/87c2krGjP7XR92qQ7",false],
+    "Cooke A Lot" : [42.999437, -78.793216 ,true,"https://goo.gl/maps/Pk3XbMH2YNsZXkLj9",false],
+    "Cooke B Lot" : [42.998715, -78.793237, true,"https://goo.gl/maps/dyMAes4BksPJ8FdZ7",false],
+    "Crofts Lot" : [42.994807, -78.797078, true,"https://goo.gl/maps/GKfubFAw3Gjcy8pi7",false],
+    "Furnas Lot" : [43.00245, -78.786328, true,"https://goo.gl/maps/sTBnFFY6f53cRRdH6", true],
+    "Fronczak Lot" : [43.002425, -78.791424, true,"https://goo.gl/maps/VchkxMUY14nd4Nwq8",false],
+    "Hochstetter A Lot" : [42.998809, -78.791585, true,"https://goo.gl/maps/9V5vYTvcDN2eT8Xx9", true],
+    "Hochstetter B Lot" : [42.99859, -78.790212, false,"https://goo.gl/maps/bUJnnMTF1eFhpxRc7",false],
+    "Governors A Lot" :[43.00234,-78.790877,true,"https://goo.gl/maps/28n38jT1GiTqfQqF9", true],
+    "Jacobs B Lot": [42.998401,-78.7871, true,"https://goo.gl/maps/qGXTVax2x9d6YNFj9",false],
+    "Jacobs A Lot": [42.998511,-78.788302, true,"https://goo.gl/maps/xgxEpQ4t3bUt9z6x6",true],
+    "Jacobs C Lot": [42.998574,-78.786113, true,"https://goo.gl/maps/Ha3VeTLKDvSWJCTB9",false],
+    "Jarvis A Lot" : [43.003721, -78.788517, false,"https://goo.gl/maps/DJrwMmxD3FNmkjr96",false],
+    "Jarvis B Lot" : [43.003972, -78.786929, true,"https://goo.gl/maps/pcVVLd6PnjKf1hZa9",false],
+    "Ketter Lot" :  [43.002466, -78.788838, true,"https://goo.gl/maps/6h7i4edeea1Jd2BKA",false],
+    "Park Hall Lot" : [42.999657, -78.788688, true,"https://goo.gl/maps/5ja1ArexJ4VQQ3UX7",false],
+    "Slee A Lot" : [42.99848, -78.783517, true,"https://goo.gl/maps/3eavzUUU53MVKbmt9",false],
+    "Slee B Lot" : [42.999374, -78.783474, true,"https://goo.gl/maps/qDQPpq8UavPQi75q6",false]
 }
 
 function UserDashboard(){
     const [available, setAvailable] = useState(true);
     const [free, setFree] = useState(false);
     const [paid, setPaid] = useState(false);
+    const [faculty, setFaculty] = useState(false);
 
     const [nearByLotFree, setNearByLotFree] = useState(Object.keys(parking)[0]);
     const [nearByLotPaid, setNearByLotPaid] = useState(Object.keys(parking)[0]);
+    const [nearByFacultyLot, setNearByFacultyLot] = useState(Object.keys(parking)[0]);
 
     const [times, setTime] = useState(new Date().toLocaleTimeString());
 
@@ -44,20 +50,14 @@ function UserDashboard(){
         setCurrentPosition(currentPosition);
     };
 
-    // useEffect(() => {
-    //     let secTimer = setInterval(() => {
-    //         setTime(new Date().getMinutes)
-    //     }, 1000)
-    //
-    //     return () => clearInterval(secTimer);
-    // },[]);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(success);
-        findNearFreeLots(free,paid)
+        findNearFreeLots(free,paid,faculty)
     })
 
-    function findNearFreeLots(free,paid){
+    function findNearFreeLots(free,paid,faculty){
+        console.log(faculty);
         let distance = {};
         for(let [keys,values] of Object.entries(parking)){
             if(free){
@@ -65,20 +65,29 @@ function UserDashboard(){
                     distance[keys] = distanceFormula(currentPosition.lat, currentPosition.lng, values[0], values[1]);
                 }
             }
-            else if(paid){
-                if(!values[2]){
+            else if(paid) {
+                if (!values[2]) {
                     distance[keys] = distanceFormula(currentPosition.lat, currentPosition.lng, values[0], values[1]);
                 }
             }
+            else if(faculty){
+                if(values[4]){
+                    distance[keys] = distanceFormula(currentPosition.lat, currentPosition.lng, values[0], values[1]);
+                }
+            }
+
         }
+
         distance = Object.entries(distance).sort((a, b) => a[1] - b[1])
         if (free){
             setNearByLotFree(distance[0][0]);
-            console.log(distance);
         }
         else if(paid){
             setNearByLotPaid(distance[0][0]);
-            console.log(distance);
+        }
+        else if(faculty){
+            console.log(distance)
+            setNearByFacultyLot(distance[0][0]);
         }
         return 0;
     }
@@ -108,28 +117,28 @@ function UserDashboard(){
     }
 
     useEffect(()=>{
-        getParkingLotsData();
         setInterval(getTime,1000);
     });
-    function getParkingLotsData(){
-        fetch('/dashboard')
-            .then(res => res.json())
-            .then(userData => {
-                for (let details in userData[0]){
-                    console.log(userData[0][details])
-                }
-            })
-    }
 
     function handleLots(setLots){
         if (setLots === "free"){
             setFree(!free);
+            setFaculty(false);
         }
         else if (setLots === "paid"){
             setPaid(!paid);
+            setFaculty(false);
+        }
+        else if(setLots === "faculty"){
+            setFaculty(!faculty);
+
+            setAvailable(false);
+            setPaid(false);
+            setFree(false);
         }
         else{
             setAvailable(!available);
+            setFaculty(false);
         }
 
         return 0;
@@ -158,13 +167,14 @@ function UserDashboard(){
                             <button onClick={()=>handleLots("available")} className={available ? "dashboard-buttons active" : "dashboard-buttons"}>Available Lots</button>
                             <button onClick={()=>handleLots("free")} className={free ? "dashboard-buttons active" : "dashboard-buttons"}>Free Lots</button>
                             <button onClick={()=>handleLots("paid")} className={paid ? "dashboard-buttons active" : "dashboard-buttons"}>Paid Lots</button>
+                            <button onClick={()=>handleLots("faculty")} className={faculty ? "dashboard-buttons active" : "dashboard-buttons"}>Faculty Lots</button>
                         </div>
                         <div>
                             {available || free || paid ?
                                 !free && available && paid ?
                                     <div className="dashboard-parking-lots">
                                         {Object.entries(parking).map((key,value)=>{
-                                            if (!key[1][2]){
+                                            if (!key[1][2] && !key[1][4]){
                                                 return (
                                                     <div className="available-parking-lots">
                                                         <a href={key[1][3]} target={"_blank"}>
@@ -178,7 +188,7 @@ function UserDashboard(){
                                     : !paid && available && free ?
                                     <div className="dashboard-parking-lots">
                                         {Object.entries(parking).map((key,value)=>{
-                                            if (key[1][2]){
+                                            if (key[1][2] && !key[1][4]){
                                                 return (<div className="available-parking-lots">
                                                     <a href={key[1][3]} target={"_blank"}>
                                                         {key[0]}
@@ -190,7 +200,7 @@ function UserDashboard(){
                                     : paid && !free && !available ?
                                         <div className="dashboard-parking-lots">
                                             {Object.entries(parking).map((key,value)=>{
-                                                if (!key[1][2]){
+                                                if (!key[1][2] && !key[1][4]){
                                                     return (<div className="available-parking-lots">
                                                         <a href={key[1][3]} target={"_blank"}>
                                                             {key[0]}
@@ -202,7 +212,7 @@ function UserDashboard(){
                                         : free && !paid && !available ?
                                             <div className="dashboard-parking-lots">
                                                 {Object.entries(parking).map((key,value)=>{
-                                                    if (key[1][2]){
+                                                    if (key[1][2] && !key[1][4]){
                                                         return (<div className="available-parking-lots">
                                                             <a href={key[1][3]} target={"_blank"}>
                                                                 {key[0]}
@@ -213,28 +223,50 @@ function UserDashboard(){
                                             </div>
                                             : <div className="dashboard-parking-lots">
                                                 {Object.entries(parking).map((key,value)=>{
-                                                    return (<div className="available-parking-lots">
-                                                        <a href={key[1][3]} target={"_blank"}>
-                                                            {key[0]}
-                                                        </a>
-                                                    </div>)
+                                                    if(!key[1][4]){
+                                                        return (<div className="available-parking-lots">
+                                                            <a href={key[1][3]} target={"_blank"}>
+                                                                {key[0]}
+                                                            </a>
+                                                        </div>)
+                                                    }
+
 
                                                 })}
                                             </div>
                                 : <div/>
+                            }
+                            {
+                                faculty ?
+                                <div className="dashboard-parking-lots">
+                                    {Object.entries(parking).map((key,value)=>{
+                                        if(key[1][4]){
+                                        console.log(key[1][4])
+                                        return (<div className="available-parking-lots">
+                                        <a href={key[1][3]} target={"_blank"}>
+                                    {key[0]}
+                                        </a>
+                                        </div>)
+                                    }
+                                    })}
+                                </div> : <></>
                             }
                         </div>
                     </div>
                 </div>
                 <div>
                     {
-                        free && !paid ?
-                            <GoogleMaps frees={true} availables={true} paids={false} nearbyFree={nearByLotFree} nearbyPaid={""} />
+                        free && !paid && !faculty ?
+                            <GoogleMaps frees={true} availables={true} paids={false} faculty={false} nearbyFree={nearByLotFree} nearbyPaid={""} nearbyFaculty={""} />
                         :
-                        paid && !free ?
-                            <GoogleMaps frees={false} availables={true} paids={true} nearbyFree={""} nearbyPaid={nearByLotPaid} />
+                        paid && !free && !faculty?
+                            <GoogleMaps frees={false} availables={true} paids={true} faculty={false} nearbyFree={""} nearbyPaid={nearByLotPaid} nearbyFaculty={""} />
                         :
-                            <GoogleMaps frees={true} availables={true} paids={true} nearbyFree={""} nearbyPaid={""} />
+                        faculty && !free && !paid && !available ?
+                            <GoogleMaps frees={false} availables={false} paids={false} faculty={true} nearbyFree={""} nearbyPaid={""} nearbyFaculty={nearByFacultyLot} />
+                        :
+                        !faculty &&
+                            <GoogleMaps frees={true} availables={true} paids={true} faculty={false} nearbyFree={""} nearbyPaid={""} nearbyFaculty={""} />
                     }
 
                 </div>
