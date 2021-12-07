a5f9f5688460bb345f5f76cc3755b353ffa176ae
swisstackle <alainsch@buffalo.edu>
2021-12-07 16:07:36 -0500

    tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+
 import index1 from './components/pages/home.jsx';
 import dashboard from './components/pages/dashboard.jsx';
+import {useAuth} from "context/AuthContext";
+
+
+const projectId = 'firebasetest';
+process.env.GCLOUD_PROJECT = projectId;
+process.env.FIRESTORE_EMULATOE_HOST = "localhost:8080";
+let app2 = admin.initializeApp();
+let db = firebase.firestore(app2);
 
 it('Home Page test', () => {
     const tree = renderer.create(<index1 />).toJSON();
@@ -13,4 +22,19 @@ it('dashboard test', () => {
     const tree = renderer.create(<dashboard />).toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+it('dashboard test', () => {
+    const {login} = useAuth();
+    try {
+
+        login(usernameRef.current.value, passwordRef.current.value)
+        history.push('/dashboard')
+    } catch{
+        return setError("Failed to login")
+    }
+
+    const tree = renderer.create(<dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
 
